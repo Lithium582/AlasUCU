158e717b24f13b07350b96b94090e960abf721ae
Lithium Cinco Ocho Dos <angelmamberto15@hotmail.com>
2017-10-07 17:58:48 -0300

    Segundo COMMIT

--- AlasUCU/src/AlasUCU/AlasUCU.java
+++ AlasUCU/src/AlasUCU/AlasUCU.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package AlasUCU;
+import UCUGrafos.*;
 
 /**
  *
@@ -15,7 +16,20 @@ public class AlasUCU {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        TGrafoDirigido gd = (TGrafoDirigido) UtilGrafos.cargarGrafo("src/aeropuertos_1.txt","src/conexiones_1.txt",
+                false, TGrafoDirigido.class);
+
+        Object[] etiquetasarray = gd.getEtiquetasOrdenado();
+
+        Double[][] matriz = UtilGrafos.obtenerMatrizCostos(gd.getVertices());
+        UtilGrafos.imprimirMatrizMejorado(matriz, gd.getVertices(), "Matriz");
+        Double[][] mfloyd = gd.floyd();
+        UtilGrafos.imprimirMatrizMejorado(mfloyd, gd.getVertices(), "Matriz luego de FLOYD");
+        for (int i = 0; i < etiquetasarray.length; i++) {
+            System.out.println("excentricidad de " + etiquetasarray[i] + " : " + gd.obtenerExcentricidad((Comparable) etiquetasarray[i]));
+        }
+        System.out.println();
+        System.out.println("Centro del grafo: " + gd.centroDelGrafo());
     }
     
 }
